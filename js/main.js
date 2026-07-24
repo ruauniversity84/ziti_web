df8ac5c9b25d5ec942fdceb990377c6b65db7b39
ruauniversity84 <rua.university.84@gmail.com>
2026-07-24 12:52:50 +0900

    全体的な整形

--- js/main.js
+++ js/main.js
@@ -1,25 +1,16 @@
-const menuBtn = document.querySelector(".menu-btn");
 const floatingBtn = document.getElementById("floating-menu-btn");
 const closeBtn = document.querySelector(".close-btn");
 const nav = document.querySelector("nav");
 const header = document.querySelector("header");
 const menuLinks = document.querySelectorAll("nav a");
 
-// メニューを閉じる共通処理
+// メニューを閉じる
 function closeMenu() {
     nav.classList.remove("active");
-
-    // ヘッダーが見えていなければ固定ボタンを再表示
-    if (header.getBoundingClientRect().bottom <= 0) {
-        floatingBtn.classList.add("show");
-    }
+    floatingBtn.classList.add("show");
 }
 
-// ヘッダーの☰
-menuBtn.addEventListener("click", () => {
-    nav.classList.add("active");
-    floatingBtn.classList.remove("show");
-});
+floatingBtn.classList.add("show");
 
 // 固定☰
 floatingBtn.addEventListener("click", () => {
@@ -33,13 +24,4 @@ closeBtn.addEventListener("click", closeMenu);
 // メニューをクリックしたら閉じる
 menuLinks.forEach(link => {
     link.addEventListener("click", closeMenu);
-});
-
-// スクロールで表示
-window.addEventListener("scroll", () => {
-    if (header.getBoundingClientRect().bottom <= 0) {
-        floatingBtn.classList.add("show");
-    } else {
-        floatingBtn.classList.remove("show");
-    }
 });
